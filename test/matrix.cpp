c16aef7567dfbcb2b0a91cef2a4c399f9b72aad6
schansge <grosser.knuff@googlemail.com>
2013-02-17 22:10:43 +0100

    matrix test class added

--- test/matrix.cpp
+++ test/matrix.cpp
@@ -2,26 +2,53 @@
 #include "gtest/gtest.h"
 #include "../include/fasteit.h"
 
-// check matrix for equality
-int matrixCompare(std::shared_ptr<fastEIT::Matrix<fastEIT::dtype::real>> A,
-    std::shared_ptr<fastEIT::Matrix<fastEIT::dtype::real>> B) {
-    // check sizes
-    if ((A->rows() != B->rows()) || (A->columns() != B->columns())) {
-        throw std::invalid_argument("matrixCompare: matrices not of same size");
+// test class
+class MatrixTest :
+    public ::testing::Test {
+protected:
+    // compare matrices on CPU
+    template <
+        class type
+    >
+    int matrixCompare(std::shared_ptr<fastEIT::Matrix<type>> A,
+        std::shared_ptr<fastEIT::Matrix<type>> B) {
+        // check sizes
+        if ((A->rows() != B->rows()) || (A->columns() != B->columns())) {
+            throw std::invalid_argument("matrixCompare: matrices not of same size");
+        }
+
+        // compare matrices
+        int result = 0;
+        for (fastEIT::dtype::index row = 0; row < A->rows(); ++row) {
+            for (fastEIT::dtype::index column = 0; column < A->columns(); ++column) {
+                result += (*B)(row, column) - (*A)(row, column);
+            }
+        }
+
+        return result;
     }
 
-    // compare matrices
-    int result = 0;
-    for (fastEIT::dtype::index row = 0; row < A->rows(); ++row) {
-        for (fastEIT::dtype::index column = 0; column < A->columns(); ++column) {
-            result += (*B)(row, column) - (*A)(row, column);
+    // add matrices on CPU
+    template <
+        class type
+    >
+    void add(std::shared_ptr<fastEIT::Matrix<type>> A,
+        std::shared_ptr<fastEIT::Matrix<type>> B) {
+        // check sizes
+        if ((A->rows() != B->rows()) || (A->columns() != B->columns())) {
+            throw std::invalid_argument("MatrixTest::add: matrices not of same size");
+        }
+
+        // add matrices
+        for (fastEIT::dtype::index row = 0; row < A->rows(); ++row) {
+            for (fastEIT::dtype::index column = 0; column < A->columns(); ++column) {
+                (*A)(row, column) += (*B)(row, column);
+            }
         }
     }
+};
 
-    return result;
-}
-
-TEST(MatrixTest, Add) {
+TEST_F(MatrixTest, Add) {
     // init random generator
     std::default_random_engine generator;
     std::uniform_real_distribution<fastEIT::dtype::real> distribution(-10.0, 10.0);
@@ -46,16 +73,12 @@ TEST(MatrixTest, Add) {
     A->add(B, nullptr);
 
     // add on CPU
-    for (fastEIT::dtype::index row = 0; row < A->rows(); ++row) {
-        for (fastEIT::dtype::index column = 0; column < A->columns(); ++column) {
-             (*B)(row, column) += (*A)(row, column);
-        }
-    }
+    this->add(B, A);
 
     // copy to host
     A->copyToHost(nullptr);
     cudaStreamSynchronize(nullptr);
 
     // compare
-    EXPECT_EQ(matrixCompare(A, B), 0);
-}
+    EXPECT_EQ(this->matrixCompare(A, B), 0);
+};
