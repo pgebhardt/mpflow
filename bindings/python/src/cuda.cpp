300b62ca1b66fb258d184fa8fcd0ab10e8ab28ef
Patrik Gebhardt <patrik.gebhardt@rub.de>
2013-04-02 12:28:58 +0200

    further cuda methods added

--- bindings/python/src/cuda.cpp
+++ bindings/python/src/cuda.cpp
@@ -17,6 +17,16 @@ void cudaStreamSynchronize_wrapper(cudaStream_t stream) {
     cudaStreamSynchronize(stream);
 }
 
+void cudaSetDevice_wrapper(int device_id) {
+    cudaSetDevice(device_id);
+}
+
+int cudaGetDeviceCount_wrapper() {
+    int count = 0;
+    cudaGetDeviceCount(&count);
+    return count;
+}
+
 void pyfasteit::export_cuda() {
     // expose this module as part of fasteit package
     object module(handle<>(borrowed(PyImport_AddModule("fasteit.cuda"))));
@@ -28,6 +38,8 @@ void pyfasteit::export_cuda() {
     def("cublas_handle", &cublasCreate_wrapper,
         return_value_policy<return_opaque_pointer>());
     def("stream_synchronize", &cudaStreamSynchronize_wrapper);
+    def("set_device", &cudaSetDevice_wrapper);
+    def("device_count", &cudaGetDeviceCount_wrapper);
 
     // reset scope
     scope();
