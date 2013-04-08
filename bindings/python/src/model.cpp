b1ea23aebfb4815024f6118e92e9600377011753
Patrik Gebhardt <patrik.gebhardt@rub.de>
2013-04-08 14:55:52 +0200

    new model type updated

--- bindings/python/src/model.cpp
+++ bindings/python/src/model.cpp
@@ -1,32 +1,47 @@
 #include <pyfasteit/pyfasteit.hpp>
 using namespace boost::python;
 
+void wrap_model(const char* name) {
+    class_<fastEIT::Model_base,
+        std::shared_ptr<fastEIT::Model_base>>(
+        name, init<
+            std::shared_ptr<fastEIT::Mesh>, std::shared_ptr<fastEIT::Electrodes>,
+            std::shared_ptr<fastEIT::source::Source>, fastEIT::dtype::real,
+            fastEIT::dtype::size>())
+    .def("update", &fastEIT::Model_base::update)
+    .add_property("mesh", &fastEIT::Model_base::mesh)
+    .add_property("electrodes", &fastEIT::Model_base::electrodes)
+    .add_property("source", &fastEIT::Model_base::source)
+    .def("system_matrix", &fastEIT::Model_base::system_matrix)
+    .def("potential", &fastEIT::Model_base::potential)
+    .add_property("jacobian", &fastEIT::Model_base::jacobian)
+    .add_property("s_matrix", &fastEIT::Model_base::s_matrix)
+    .add_property("r_matrix", &fastEIT::Model_base::r_matrix)
+    .add_property("connectivity_matrix", &fastEIT::Model_base::connectivity_matrix)
+    .add_property("elemental_s_matrix", &fastEIT::Model_base::elemental_s_matrix)
+    .add_property("elemental_r_matrix", &fastEIT::Model_base::elemental_r_matrix)
+    .add_property("elemental_jacobian_matrix",
+        &fastEIT::Model_base::elemental_jacobian_matrix)
+    .add_property("sigma_ref", &fastEIT::Model_base::sigma_ref)
+    .add_property("components_count", &fastEIT::Model_base::components_count);
+}
+
 template <
     class basis_function_type
 >
-void wrap_model(const char* name) {
+void wrap_derived_model(const char* name) {
     class_<fastEIT::Model<basis_function_type>,
-        std::shared_ptr<fastEIT::Model<basis_function_type>>>(
+        std::shared_ptr<fastEIT::Model<basis_function_type>>,
+        bases<fastEIT::Model_base>>(
         name, init<
             std::shared_ptr<fastEIT::Mesh>, std::shared_ptr<fastEIT::Electrodes>,
-            std::shared_ptr<fastEIT::source::Source<basis_function_type>>,
-            fastEIT::dtype::real, fastEIT::dtype::size, cublasHandle_t, cudaStream_t>())
-    .def("update", &fastEIT::Model<basis_function_type>::update)
-    .add_property("mesh", &fastEIT::Model<basis_function_type>::mesh)
-    .add_property("electrodes", &fastEIT::Model<basis_function_type>::electrodes)
-    .add_property("source", &fastEIT::Model<basis_function_type>::source)
-    .def("system_matrix", &fastEIT::Model<basis_function_type>::system_matrix)
-    .def("potential", &fastEIT::Model<basis_function_type>::potential)
-    .add_property("jacobian", &fastEIT::Model<basis_function_type>::jacobian)
-    .add_property("s_matrix", &fastEIT::Model<basis_function_type>::s_matrix)
-    .add_property("r_matrix", &fastEIT::Model<basis_function_type>::r_matrix)
-    .add_property("connectivity_matrix", &fastEIT::Model<basis_function_type>::connectivity_matrix)
-    .add_property("elemental_s_matrix", &fastEIT::Model<basis_function_type>::elemental_s_matrix)
-    .add_property("elemental_r_matrix", &fastEIT::Model<basis_function_type>::elemental_r_matrix)
-    .add_property("elemental_jacobian_matrix",
-        &fastEIT::Model<basis_function_type>::elemental_jacobian_matrix)
-    .add_property("sigma_ref", &fastEIT::Model<basis_function_type>::sigma_ref)
-    .add_property("components_count", &fastEIT::Model<basis_function_type>::components_count);
+            std::shared_ptr<fastEIT::source::Source>, fastEIT::dtype::real,
+            fastEIT::dtype::size, cublasHandle_t, cudaStream_t>());
+
+    implicitly_convertible<fastEIT::Model<basis_function_type>,
+        fastEIT::Model_base>();
+    implicitly_convertible<std::shared_ptr<fastEIT::Model<basis_function_type>>,
+        std::shared_ptr<fastEIT::Model_base>>();
 }
 
 void pyfasteit::export_model() {
@@ -35,8 +50,12 @@ void pyfasteit::export_model() {
     scope().attr("model") = module;
     scope sub_module = module;
 
-    wrap_model<fastEIT::basis::Linear>("Linear");
-    wrap_model<fastEIT::basis::Quadratic>("Quadratic");
+    // wrap model base class
+    wrap_model("Model");
+
+    // wrap derived classes
+    wrap_derived_model<fastEIT::basis::Linear>("Linear");
+    wrap_derived_model<fastEIT::basis::Quadratic>("Quadratic");
 
     // reset scope
     scope();
--- bindings/python/src/source.cpp
+++ bindings/python/src/source.cpp
@@ -1,34 +1,28 @@
 #include <pyfasteit/pyfasteit.hpp>
 using namespace boost::python;
 
-template <
-    class basis_function_type
->
 void wrap_source(const char* name) {
-    class_<fastEIT::source::Source<basis_function_type>,
-        std::shared_ptr<fastEIT::source::Source<basis_function_type>>>(
+    class_<fastEIT::source::Source,
+        std::shared_ptr<fastEIT::source::Source>>(
         name, init<std::string,
             fastEIT::dtype::real, std::shared_ptr<fastEIT::Mesh>,
             std::shared_ptr<fastEIT::Electrodes>, fastEIT::dtype::size,
             std::shared_ptr<fastEIT::Matrix<fastEIT::dtype::real>>,
             std::shared_ptr<fastEIT::Matrix<fastEIT::dtype::real>>,
             cublasHandle_t, cudaStream_t>())
-    .def("update_excitation", &fastEIT::source::Source<basis_function_type>::updateExcitation)
-    .add_property("drive_pattern", &fastEIT::source::Source<basis_function_type>::drive_pattern)
-    .add_property("measurement_pattern",
-        &fastEIT::source::Source<basis_function_type>::measurement_pattern)
-    .add_property("pattern", &fastEIT::source::Source<basis_function_type>::pattern)
-    .add_property("elemental_pattern",
-        &fastEIT::source::Source<basis_function_type>::elemental_pattern)
-    .add_property("d_matrix", &fastEIT::source::Source<basis_function_type>::d_matrix)
-    .add_property("w_matrix", &fastEIT::source::Source<basis_function_type>::w_matrix)
-    .add_property("x_matrix", &fastEIT::source::Source<basis_function_type>::x_matrix)
-    .add_property("z_matrix", &fastEIT::source::Source<basis_function_type>::z_matrix)
-    .def("excitation", &fastEIT::source::Source<basis_function_type>::excitation)
-    .add_property("drive_count", &fastEIT::source::Source<basis_function_type>::drive_count)
-    .add_property("measurement_count",
-        &fastEIT::source::Source<basis_function_type>::measurement_count)
-    .add_property("value", &fastEIT::source::Source<basis_function_type>::value);
+    .def("update_excitation", &fastEIT::source::Source::updateExcitation)
+    .add_property("drive_pattern", &fastEIT::source::Source::drive_pattern)
+    .add_property("measurement_pattern", &fastEIT::source::Source::measurement_pattern)
+    .add_property("pattern", &fastEIT::source::Source::pattern)
+    .add_property("elemental_pattern", &fastEIT::source::Source::elemental_pattern)
+    .add_property("d_matrix", &fastEIT::source::Source::d_matrix)
+    .add_property("w_matrix", &fastEIT::source::Source::w_matrix)
+    .add_property("x_matrix", &fastEIT::source::Source::x_matrix)
+    .add_property("z_matrix", &fastEIT::source::Source::z_matrix)
+    .def("excitation", &fastEIT::source::Source::excitation)
+    .add_property("drive_count", &fastEIT::source::Source::drive_count)
+    .add_property("measurement_count", &fastEIT::source::Source::measurement_count)
+    .add_property("value", &fastEIT::source::Source::value);
 }
 
 template <
@@ -37,7 +31,7 @@ template <
 >
 void wrap_derived_source(const char* name) {
     class_<source_type<basis_function_type>, std::shared_ptr<source_type<basis_function_type>>,
-        bases<fastEIT::source::Source<basis_function_type>>>(
+        bases<fastEIT::source::Source>>(
         name, init<
             fastEIT::dtype::real, std::shared_ptr<fastEIT::Mesh>,
             std::shared_ptr<fastEIT::Electrodes>, fastEIT::dtype::size,
@@ -46,9 +40,9 @@ void wrap_derived_source(const char* name) {
             cublasHandle_t, cudaStream_t>());
 
     implicitly_convertible<source_type<basis_function_type>,
-        fastEIT::source::Source<basis_function_type>>();
+        fastEIT::source::Source>();
     implicitly_convertible<std::shared_ptr<source_type<basis_function_type>>,
-        std::shared_ptr<fastEIT::source::Source<basis_function_type>>>();
+        std::shared_ptr<fastEIT::source::Source>>();
 }
 
 void pyfasteit::export_source() {
@@ -58,7 +52,7 @@ void pyfasteit::export_source() {
     scope sub_module = module;
 
     // wrap source base class
-    wrap_source<fastEIT::basis::Linear>("Source");
+    wrap_source("Source");
 
     // wrap derived classes
     wrap_derived_source<fastEIT::source::Current,
