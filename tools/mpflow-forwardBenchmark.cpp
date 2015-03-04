82cc7415455304263d813a18fc3c55dc1b346100
Patrik Gebhardt <patrik.gebhardt@rub.de>
2015-03-04 15:42:03 +0100

    added benchmark tool for forward solving with different grid densities

---
+++ tools/mpflow-forwardBenchmark.cpp
@@ -0,0 +1,100 @@
+#include <distmesh/distmesh.h>
+#include <mpflow/mpflow.h>
+#include "stringtools/all.hpp"
+#include "high_precision_time.h"
+
+using namespace mpFlow;
+
+// helper function to create unit matrix
+template <class type>
+std::shared_ptr<numeric::Matrix<type>> eye(dtype::index size, cudaStream_t cudaStream) {
+    auto matrix = std::make_shared<numeric::Matrix<type>>(size, size, cudaStream);
+    for (dtype::index i = 0; i < size; ++i) {
+        (*matrix)(i, i) = 1;
+    }
+    matrix->copyToDevice(cudaStream);
+
+    return matrix;
+}
+
+int main(int argc, char* argv[]) {
+    HighPrecisionTime time;
+
+    // print out mpFlow version for refernce
+    str::print(str::format("mpFlow version: %s")(version::getVersionString()));
+
+    // init cuda
+    cudaStream_t cudaStream = nullptr;
+    cublasHandle_t cublasHandle = nullptr;
+    cudaSetDevice(1);
+    cublasCreate(&cublasHandle);
+    cudaStreamCreate(&cudaStream);
+
+    // solve complete forward problem for a common ECT measurement setup for increasing
+    // mesh density
+    dtype::real density = 1.0;
+    for (dtype::index i = 0; i < 16; ++i) {
+        // Create Mesh using libdistmesh
+        time.restart();
+        str::print("----------------------------------------------------");
+        str::print(str::format("Create mesh with density: %f")(density));
+
+        auto dist_mesh = distmesh::distmesh(distmesh::distance_function::circular(1.0),
+            density, 1.0, 1.1 * distmesh::bounding_box(2));
+        auto boundary = distmesh::boundedges(std::get<1>(dist_mesh));
+
+        str::print(str::format("Mesh created with %d nodes and %d elements")(
+            std::get<0>(dist_mesh).rows(), std::get<1>(dist_mesh).rows()));
+        str::print(str::format("Time: %f ms")(time.elapsed() * 1e3));
+
+        // update density
+        density /= std::sqrt(2.0);
+
+        // create mpflow mesh object
+        auto mesh = std::make_shared<numeric::IrregularMesh>(
+            numeric::matrix::fromEigen<dtype::real, distmesh::dtype::real>(std::get<0>(dist_mesh), cudaStream),
+            numeric::matrix::fromEigen<dtype::index, distmesh::dtype::index>(std::get<1>(dist_mesh), cudaStream),
+            numeric::matrix::fromEigen<dtype::index, distmesh::dtype::index>(boundary, cudaStream), 1.0, 1.0);
+
+        // create electrodes
+        auto electrodes = FEM::boundaryDescriptor::circularBoundary(
+            16, std::make_tuple(0.03, 0.1), 1.0, 0.0);
+
+        // create pattern
+        auto drivePattern = eye<dtype::real>(electrodes->count, cudaStream);
+        auto measurementPattern = eye<dtype::real>(electrodes->count, cudaStream);
+
+        // create source
+        auto source = std::make_shared<FEM::SourceDescriptor>(
+            FEM::SourceDescriptor::Type::Fixed, 1.0, electrodes,
+            drivePattern, measurementPattern, cudaStream);
+
+        // create equation
+        time.restart();
+        str::print("--------------------------");
+        str::print("Create equation model class");
+
+        auto equation = std::make_shared<FEM::Equation<dtype::real, FEM::basis::Linear>>(
+            mesh, electrodes, 1.0, cudaStream);
+
+        cudaStreamSynchronize(cudaStream);
+        str::print(str::format("Time: %f ms")(time.elapsed() * 1e3));
+
+        // Create forward solver and solve potential
+        str::print("--------------------------");
+        str::print("Solve electrical potential for all excitations");
+
+        auto forwardSolver = std::make_shared<EIT::ForwardSolver<FEM::basis::Linear,
+            numeric::BiCGSTAB>>(equation, source, 1, cublasHandle, cudaStream);
+        auto gamma = std::make_shared<numeric::Matrix<dtype::real>>(mesh->elements->rows, 1,
+            cudaStream);
+
+        time.restart();
+        forwardSolver->solve(gamma, mesh->nodes->rows, cublasHandle, cudaStream);
+
+        cudaStreamSynchronize(cudaStream);
+        str::print(str::format("Time: %f ms")(time.elapsed() * 1e3));
+    }
+
+    return EXIT_SUCCESS;
+}
